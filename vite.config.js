3c0d98f39d83c9ee238d88f21870327c0b30c120
Nikita828 <chuklanov.nik@mail.ru>
2026-02-11 16:14:23 +0300

    release version 0.0.0.3

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
+  
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    
     rollupOptions: {
-      external: ['phaser'], // Phaser теперь внешний
+      external: ['phaser']
     },
     
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild',
+    sourcemap: false,
+    
+    // Копировать только нужные файлы из public
+    copyPublicDir: true
   },
+  
+  // Исключаем моки из копирования
+  publicDir: 'public',
   
   server: {
     port: 5173,
